e7601eb513296ffd48cc3d1fe45912436d8766e1
Aristides Neto <aristidesjbn@gmail.com>
2026-02-25 12:40:59 -0300

    primeiro commit: Atividade da Bolsa PC em React e Next.js

--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,73 @@
-import Image from "next/image";
+"use client";
+
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const [time, setTime] = useState(new Date());
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setTime(new Date());
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  const seconds = time.getSeconds();
+  const minutes = time.getMinutes();
+  const hours = time.getHours();
+
+  const secondAngle = seconds * 6; 
+  const minuteAngle = minutes * 6;
+  const hourAngle = hours * 30 + minutes * 0.5; 
+
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
+    <main className="flex items-center justify-center h-screen bg-gray-200">
+      <div className="relative w-88 h-88 bg-white rounded-full border-4 border-black flex items-center justify-center">
+
+      {Array.from({ length: 12 }).map((_, i) => {
+          const number = i + 1;
+          const angle = number * 30;
+
+          return (
+            <div
+              key={number}
+              className="absolute text-lg font-bold text-black"
+              style={{
+                transform: `rotate(${angle}deg) translateY(-150px) rotate(-${angle}deg)`,
+              }}
+            >
+              {number}
+            </div>
+          );
+        })}
+
+        <div
+          className="absolute w-2 h-18 bg-black origin-bottom"
+          style={{
+            bottom: "50%",
+            transform: `rotate(${hourAngle}deg)`
+          }}
         />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
-    </div>
+
+        <div
+          className="absolute w-1.5 h-26 bg-black origin-bottom"
+          style={{
+            bottom: "50%",
+            transform: `rotate(${minuteAngle}deg)`
+          }}
+        />
+
+        <div
+          className="absolute w-1 h-30 bg-red-500 origin-bottom"
+          style={{
+            bottom: "50%",
+            transform: `rotate(${secondAngle}deg)`
+          }}
+        />
+
+        <div className="w-4 h-4 bg-black rounded-full z-10" />
+      </div>
+    </main>
   );
 }
